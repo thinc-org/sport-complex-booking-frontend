82e9f9ba0cc740b94e93589b1286392b8139d236
sad7898 <siradhee_k@hotmail.com>
2020-09-21 13:13:21 +0700

    first commit

---
+++ my-app/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<div>hello</div>,document.getElementById('root'))
