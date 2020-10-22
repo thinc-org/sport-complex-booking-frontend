fe3e78e550fa3f1f84a00c74a785c7c63145ba2e
saenyakorn <saneyakorn@gmail.com>
2020-10-22 13:14:04 +0700

    setup: project

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Main from './components/pages/MainComponent';
+import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './store/configureStore';
+import FrontLogin from './components/pages/front-login';
+import NavigationBar from './components/ui/navbar';
+const store = ConfigureStore();
+
+function App() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <NavigationBar/>
+                <Switch>
+                    <Route path="/login">
+                        <FrontLogin/>
+                    </Route>
+                    <Route path="/">
+                        <div>
+                            <Main/>
+                        </div>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+
+        
+    )
+}
+
+export default App;
