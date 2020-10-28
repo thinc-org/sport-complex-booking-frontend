85872d1e009d0e3bd3c71670a13bd64aec116713
Veerin Phana-ngam <beam7684@gmail.com>
2020-10-28 23:09:58 +0700

    minor update

--- src/core/routes/index.route.tsx
+++ src/core/routes/index.route.tsx
@@ -2,6 +2,11 @@ import React from "react"
 import Switch from "react-bootstrap/esm/Switch"
 import { Route } from "react-router"
 
+import ListOfAllUsers from '../../components/pages/ListOfAllUsers';
+import CUInfo from "../../components/pages/CUInfo";
+import AddUser from "../../components/pages/AddUser";
+// import SatitInfo from "../../components/pages/SatitInfo"
+
 export default function MainRoute() {
   return (
     <Switch>
@@ -17,6 +22,12 @@ export default function MainRoute() {
       <Route exact path="/profile">
         {/* Profile Page */}
       </Route>
+
+      <Route exact path="/listOfAllUsers" component={ListOfAllUsers} />
+      <Route exact path="/CUInfo/:username" component={CUInfo} />
+      {/* <Route exact path="/SatitInfo/:username" component={SatitInfo} />
+      <Route exact path="/UserInfo/:username" component={UserInfo} /> */}
+      <Route exact path="/addUser" component={AddUser} />
     </Switch>
   )
 }
