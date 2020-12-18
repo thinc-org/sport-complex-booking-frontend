5dcf90a93f1dd399d910bb828c643dd2a45265fd
Beam <beam7684@gmail.com>
2020-12-18 23:06:14 +0700

    bug fixes at verification approval page

--- src/core/components/pages/staff-pages/verification-approval-pages/VerifyInfo.tsx
+++ src/core/components/pages/staff-pages/verification-approval-pages/VerifyInfo.tsx
@@ -159,6 +159,7 @@ const VerifyInfo: FunctionComponent<RouteComponentProps<{ _id: string }>> = (pro
     for (const name in rejectInfo) {
       if (rejectInfo[name]) rejectList.push(name)
     }
+    console.log(rejectList)
     // send request //
     client({
       method: "PATCH",
