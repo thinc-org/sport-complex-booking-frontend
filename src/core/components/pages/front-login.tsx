291d4955bfb96393517256e6feeb541c0a123f53
siradheek <siradhee_k@hotmail.com>
2020-12-18 21:45:39 +0700

    store jwt in cookie, add sso auth

--- src/core/components/pages/front-login.tsx
+++ src/core/components/pages/front-login.tsx
@@ -1,13 +1,24 @@
 import * as React from "react"
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { useEffect } from 'react'
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom';
+import { getCookie } from '../../contexts/cookieHandler'
 import FrontLoginMain from '../ui/login/login-main'
 import Footer from '../ui/footer/footer'
 import PersonalInfo from '../ui/login/personal-info-form';
 const FrontLoginPage = (props: any) => {
   let { url, path } = useRouteMatch()
+  let history = useHistory()
+  useEffect(() => {
+    if (getCookie('token')) history.push('/account')
+  }, [])
   return (
     <>
       <Switch>
+        <Route path={`${path}/sso`} component={() => {
+          window.location.href = 'https://account.it.chula.ac.th/html/login.html?service=http://localhost:3001/login';
+          return null
+        }}
+        />
         <Route path={`${path}/personal`} component={PersonalInfo} />
         <Route exact path={path} component={FrontLoginMain} />
       </Switch>
