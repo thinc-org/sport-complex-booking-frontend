4c7d61120a92ed65cdad2e5424420f09bf635036
siradheek <siradhee_k@hotmail.com>
2020-12-19 16:44:32 +0700

    add authentication: AuthContext, add backend calls and sso on login page

--- src/core/controllers/auth.controller.tsx
+++ src/core/controllers/auth.controller.tsx
@@ -1,23 +1,25 @@
 import React, { useContext, useMemo, useState } from "react"
-import Axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { getCookie } from '../contexts/cookieHandler'
 interface AuthContextConstruct {
   token: string | undefined
-  isUser: Boolean
-  isAdmin: Boolean
+  isUser: Boolean,
+  setToken: Function
+  // isAdmin: Boolean
 }
 
 export const AuthContext = React.createContext({} as AuthContextConstruct)
 
 export const useAuthContext = () => useContext(AuthContext)
 
-export const validateStaff = async (token) => {
-  return true
-}
 const AuthProvider = ({ ...props }) => {
-  const [token, setToken] = useState<string>()
-  const isUser = useMemo(() => true, [])
-  const isAdmin = useMemo(() => true, [])
-  const value = { token, isUser, isAdmin }
+  const [token, setToken] = useState<string>(getCookie('token') ?? '')
+  const isUser = useMemo(() => {
+    const decodedToken = token ? jwt_decode(token) : ''
+    return decodedToken ? true : false
+  }, [token])
+  // const isAdmin = useMemo(() => true, [])
+  const value = { token, isUser, setToken }
   return <AuthContext.Provider value={value} {...props} />
 }
 
