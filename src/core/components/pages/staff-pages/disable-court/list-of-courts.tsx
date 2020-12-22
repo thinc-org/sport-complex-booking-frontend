845a6cbe8048ffb210051e6b651ec5647fea091e
siradheek <siradhee_k@hotmail.com>
2020-12-22 07:29:25 +0700

    iterative form

--- src/core/components/pages/staff-pages/disable-court/list-of-courts.tsx
+++ src/core/components/pages/staff-pages/disable-court/list-of-courts.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Form, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Form,Modal,Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form'
 import DatePicker from 'react-datepicker'
+interface Option {
+    sportType: string[],
+    courtNum: string[]
+}
 const ListOfCourts = (props) => {
+    let {register,handleSubmit,setError,errors,getValues} =useForm();
     const currentDate = new Date()
+    const [option,setOption] = useState<Option>()
     const [startDate, setStartDate] = useState<Date>(currentDate);
     const [endDate, setEndDate] = useState<Date>();
+    const fetchData = async () => {
+        await setOption({
+            sportType: ['แบดมินตัน','บาสเกตบอล','วอลเลย์บอล'],
+            courtNum: ['1','2','3']
+        })
+    }
+    useEffect(() => {
+        fetchData()
+    },[])
     const onStartDateChange = (date) => {
+        console.log('this runs')
         if (date < currentDate) {
-
+            
         }
         else setStartDate(date)
     }
@@ -18,23 +34,36 @@ const ListOfCourts = (props) => {
     }
     return (
         <div className="disable-court-wrapper px-2 py-2 mt-3">
-            <Form>
+            <Form className='disable-court-form'>
                 <div className="d-flex flex-row">
-                    <DropdownButton title="ประเภทกีฬา" className='disable-court-dropdown'>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                    <DropdownButton title="เลขคอร์ด" className='disable-court-dropdown'>
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                    <DatePicker selected={startDate} onDateChange={onStartDateChange} />
-                    <DatePicker selected={null} onDateChange={onEndDateChange} />
-
+      
+                        <Form.Label srOnly={true}>ประเภทกีฬา</Form.Label>
+                        <Form.Control name='sports' as='select' ref={register}>
+                            <option value={undefined}>ทั้งหมด</option>
+                            {option ? option['sportType'].map((val) => (
+                                <option value={val}>{val}</option>
+                            )) : <option>ประเภทกีฬา</option>}
+                        </Form.Control>
+                        <Form.Control name='courtNum' as='select' ref={register} disabled={getValues('sports') ? false: true}>
+                            <option value={undefined}>ทั้งหมด</option>
+                            {option ? option['courtNum'].map((val) => (
+                                <option value={val}>{val}</option>
+                            )) : <option>เลขคอร์ด</option>}
+                        </Form.Control>
+                    <div>
+                        <DatePicker className='form-control' selected={startDate} onChange={onStartDateChange} />
+                        <Form.Text>{errors.startDateInvalid && "วันที่ไม่ถูกต้อง"}</Form.Text>
+                    </div>
+                    <div>
+                        <DatePicker selected={null} onChange={onEndDateChange} />
+                        <Form.Text>{errors.endDateInvalid && "วันที่ไม่ถูกต้อง"}</Form.Text>
+                    </div>
+                    <Button type='submit'>
+                        ค้นหา
+                    </Button>
                 </div>
             </Form>
+            
         </div>
 
     )
