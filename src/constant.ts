7f873fd1ecb3a83fb9667e6b12e495345a53a690
siradheek <siradhee_k@hotmail.com>
2020-12-24 01:06:50 +0700

    fix logic

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 export const is_first_login = localStorage.getItem('is_first_login') == 'true'
 export const setIsFirstLogin = (value: boolean) => localStorage.setItem('is_first_login', value.toString())
-=======
-export let is_first_login = localStorage.getItem('is_first_login') == 'true' ? true : false
-export const setFirstLogin = (value: any) => localStorage.setItem('is_first_login', value)
->>>>>>> 10d4d0d... change is_first_login to boolean
