4bd42fa55f64e02e67d8a9c7f9fc073da2ef8355
siradheek <siradhee_k@hotmail.com>
2020-12-24 00:33:38 +0700

    change is_first_login to boolean

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export const is_first_login = localStorage.getItem('is_first_login')
-export const setFirstLogin = (value: string) => localStorage.setItem('is_first_login', value)
+export let is_first_login = localStorage.getItem('is_first_login') == 'true' ? true : false
+export const setFirstLogin = (value: any) => localStorage.setItem('is_first_login', value)
