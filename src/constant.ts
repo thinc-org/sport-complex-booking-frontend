8fb361004dff6880f8cd2889befe05e14028ba55
siradheek <siradhee_k@hotmail.com>
2020-12-24 01:06:50 +0700

    fix logic

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export let is_first_login = localStorage.getItem('is_first_login') == 'true' ? true : false
+export const is_first_login = localStorage.getItem('is_first_login') == 'true'
 export const setFirstLogin = (value: any) => localStorage.setItem('is_first_login', value)
