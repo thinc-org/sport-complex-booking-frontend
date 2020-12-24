a51a3b956407f87ad9635613deacdf77e56584a1
siradheek <siradhee_k@hotmail.com>
2020-12-24 15:04:33 +0700

    fix bug: personl-info page redirect to account page when user refresh, change is_first_login getter to function

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,3 @@
-
 export const getIsFirstlogin = () => localStorage.getItem('is_first_login') == 'true'
 export const setIsFirstLogin = (value: boolean) => localStorage.setItem('is_first_login', value.toString())
 
