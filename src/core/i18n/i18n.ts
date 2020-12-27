89d35e516e5ce1e85a9238de50f38ec6f74d26d5
Dhanabordee Mekintharanggur <pointmekin@gmail.com>
2020-12-27 11:21:14 +0700

    Slight changes to translation files format

--- src/core/i18n/i18n.ts
+++ src/core/i18n/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import {initReactI18next} from 'react-i18next'
-import {en} from '../../locales/en/translation.json'
-import {th} from '../../locales/th/translation.json'
+import en from '../../locales/en/translation.json'
+import th from '../../locales/th/translation.json'
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
   fallbackLng: 'en',
