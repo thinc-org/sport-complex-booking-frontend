67818ee1968c2bf28221a591f190906442be6d02
Suparuek Saetoen <suparuk1902@hotmail.com>
2021-01-01 02:08:12 +0700

    add my reservation page

---
+++ src/core/components/pages/Reservation/Reservation.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useRouteMatch, useHistory } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { client } from '../../../../axiosConfig';
+import { UserContext } from '../../../contexts/UsersContext';
+import { timeConversion } from '../Reservation/timeConversion';
+import { AxiosResponse } from 'axios';
+import { NavHeader } from '../../ui/navbar/navbarSideEffect'
+import { useTranslation } from 'react-i18next';
+
+const ReservationPage = (props: any) => {
+
+    const history = useHistory();
+
+    const [lists, setLists] = useState<any>([]);
+    const [isThaiLanguage, setIsThaiLanguage] = useState(true)
+
+    var { url, path } = useRouteMatch();
+    const userContext = useContext(UserContext);
+    const { t } = useTranslation()
+
+
+    useEffect(() => {
+        fetchData()
+        console.log('fetch data')
+    }, [])
+
+    const fetchData = async () => {
+        await client
+            .get('http://localhost:3000/myreservation', {
+
+            })
+            .then((res: AxiosResponse) => {
+                const data = res.data;
+                setLists(data.MyReservation);
+                setIsThaiLanguage(data.is_thai_language)
+            })
+            .catch((err) => {
+
+            })
+    }
+
+    const handleClick = (id: any) => {
+        console.log('button clicked')
+        return (
+
+            history.push({
+                pathname: `${path}/reservationdetail`,
+                state: {
+                    id: id,
+                    path: path
+                }
+            })
+        )
+    }
+
+    // display only when there is any reservation
+    if (true) {
+        return (
+            <>
+                <NavHeader header='My Reservation' />
+                <div className='container'>
+                    <div className='row justify-content-center mt-5'>
+                        <div className='col-12'>
+                            <Button variant='pink' onClick={() => handleClick('list.sport')} className='box-container btn' style={{ width: '100%', color: 'black', borderColor: 'transparent' }}>
+                                <div>
+                                    <h5 style={{ color: 'lightgreen', float: 'right' }}> {true ? t('checked_in') : ''} </h5>
+                                    <h5 className='mb-2'> Badminton </h5>
+                                    <h6 className='mb-0'> {t('court')}: </h6>
+                                    <h6 className='mb-0'> {t('booking_date')}: </h6>
+                                    <h6 className='mb-0'> {t('booking_time')}: </h6>
+                                </div>
+                            </Button>
+
+                            {lists.map(list => {
+                                return (
+                                    <Button variant='pink' onClick={() => handleClick('list._id')} className='box-container btn' style={{ width: '100%', color: 'black', borderColor: 'transparent' }}>
+                                        <div>
+                                            <h5 style={{ color: 'lightgreen', float: 'right' }}> {list.is_check ? t('checked_in') : ''} </h5>
+                                            <h5 className='mb-2'> {isThaiLanguage ? list.sport_name_th : list.sport_name_en} </h5>
+                                            <h6 className='mb-0'> {t('court')}: {list.court_num} </h6>
+                                            <h6 className='mb-0'> {t('booking_date')}: {list.date} </h6>
+                                            <h6 className='mb-0'> {t('booking_time')}: {list.time_slot.map(eachTimeSlot => timeConversion(eachTimeSlot))} </h6>
+                                        </div>
+                                    </Button>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <NavHeader header='My Reservation' />
+                <div className='container'>
+                    <div className='row justify-content-center mt-5 grey' style={{ fontWeight: 400 }}>
+                        You have no any reservations.
+                </div>
+                </div>
+            </>
+        )
+    }
+
+}
+
+export default ReservationPage;
---
+++ src/core/components/ui/Modals/ReservationCancelModal.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
+
+export interface ReservationCancelModalProps {
+    modalOpen: boolean,
+    handleClick: any,
+    confirmCancellation: any
+}
+
+export const ReservationCancellationModal: React.FC<ReservationCancelModalProps> = ({ modalOpen, handleClick, confirmCancellation }) => {
+    const { t } = useTranslation()
+    return (
+        <Modal show={modalOpen} className="modal" id="exampleModalCenter" tabIndex={-1} role="dialog" aria-labelledby="contained-modal-title-vcenter" centered aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content">
+                    <div className="modal-header pb-0">
+                        <h5 className="modal-title"> {t('warning')} </h5>
+                    </div>
+                    <div className="modal-body pt-1 pb-0" style={{ fontSize: '14px', fontWeight: 300 }}>
+                        {t('cancel_reservation_question')}
+                        <br />
+                        <br /> {t('cancel_reservation_condition_1')}
+                        <br /> {t('cancel_reservation_condition_2')}
+                    </div>
+                    <div className="modal-footer pt-0 pb-0">
+                        <button type="button" onClick={handleClick} className="btn" data-dismiss="modal" style={{ fontSize: '14px', fontWeight: 400 }}> {t('no')} </button>
+                        <button type="button" onClick={confirmCancellation} className="btn" style={{ fontSize: '14px', fontWeight: 400 }}> {t('sure')} </button>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    )
+}
