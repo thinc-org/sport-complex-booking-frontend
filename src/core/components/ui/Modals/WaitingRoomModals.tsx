00f821b4c6e197b11cffa34f48dd7516bb4283d2
Suparuek Saetoen <suparuk1902@hotmail.com>
2021-01-02 17:57:38 +0700

    camelCase and bug fix

--- src/core/components/ui/Modals/WaitingRoomModals.tsx
+++ src/core/components/ui/Modals/WaitingRoomModals.tsx
@@ -3,75 +3,86 @@ import { Button, Modal } from "react-bootstrap"
 import { useTranslation } from "react-i18next"
 
 export interface DetailsModalProps {
-  show: boolean,
-  setShow(show: boolean): void,
-  sportName?: string, 
-  details: WaitingRoomData,
-  date: Date,
-  times: number[],
-  formatTime(time: number): string,
+  show: boolean
+  setShow(show: boolean): void
+  sportName?: string
+  details: WaitingRoomData
+  date: Date
+  times: number[]
+  formatTime(time: number): string
   postDataToBackend(data: WaitingRoomData): void
 }
 
 export interface WaitingRoomData {
-  sport_id: string,
-  court_number: number,
-  time_slot: number[],
-  date:Date
+  sport_id: string
+  court_number: number
+  time_slot: number[]
+  date: Date
 }
 
 export const DetailsModal: React.FC<DetailsModalProps> = ({ show, setShow, sportName, details, date, times, formatTime, postDataToBackend }) => {
   const { t } = useTranslation()
-  const handlePostData = ()=> {
+  const handlePostData = () => {
     postDataToBackend(details!)
     setShow(false)
   }
   if (!show) return null
   return (
-    <Modal className="modal" show={show} onHide={()=>setShow(false)}>
+    <Modal className="modal" show={show} onHide={() => setShow(false)}>
       <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="confirmModalLabel">
-              {t("wantToCreateWaitingRoom")}
-            </h5>
-            <button type="button" className="close" onClick={() => setShow(false)}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <h4>{t("reservationDetails")}</h4>
-            <ul>
-              <li>{t("sport")}: {sportName}</li>
-              <li>{t("court")}: {details['court_number']}</li>
-              <li>{t("bookingDate")}: {date.toString().substring(0,10)}</li>
-              <li>{t("bookingTime")}: <ul>{times.map(element => {
-                return (<li key={element.toString()}>{formatTime(element)}</li>)
-              })}</ul></li>
-            </ul> 
-          </div>
-          <div className="modal-footer">
-            <Button onClick={() => setShow(false)} type="button" variant="outline-secondary" className="btn-normal">
-              {t("cancel")}
-            </Button>
-            <Button onClick={handlePostData} variant="pink" className="btn-normal">
-              {t("create")}
-            </Button>
-          </div>
+        <div className="modal-header">
+          <h5 className="modal-title" id="confirmModalLabel">
+            {t("wantToCreateWaitingRoom")}
+          </h5>
+          <button type="button" className="close" onClick={() => setShow(false)}>
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+        <div className="modal-body">
+          <h4>{t("reservationDetails")}</h4>
+          <ul>
+            <li>
+              {t("sport")}: {sportName}
+            </li>
+            <li>
+              {t("court")}: {details["court_number"]}
+            </li>
+            <li>
+              {t("bookingDate")}: {date.toString().substring(0, 10)}
+            </li>
+            <li>
+              {t("bookingTime")}:{" "}
+              <ul>
+                {times.map((element) => {
+                  return <li key={element.toString()}>{formatTime(element)}</li>
+                })}
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <Button onClick={() => setShow(false)} type="button" variant="outline-secondary" className="btn-normal">
+            {t("cancel")}
+          </Button>
+          <Button onClick={handlePostData} variant="pink" className="btn-normal">
+            {t("create")}
+          </Button>
+        </div>
+      </div>
     </Modal>
   )
 }
 
 interface CustomWaitingRoomModalProps {
-  type: "wrongAccessCodeModal" | "cantCreateWaitingRoomModal" 
+  type: "wrongAccessCodeModal" | "cantCreateWaitingRoomModal"
   show: boolean
   setShow: (value: boolean) => void
 }
 
-export const CustomWaitingRoomModal:React.FC<CustomWaitingRoomModalProps> = ({type, show, setShow}) => {
-  const { t } = useTranslation() 
+export const CustomWaitingRoomModal: React.FC<CustomWaitingRoomModalProps> = ({ type, show, setShow }) => {
+  const { t } = useTranslation()
   const message = t(type, { returnObjects: true })
-  if(!show) return null
+  if (!show) return null
   return (
     <Modal
       show={show}
@@ -80,14 +91,14 @@ export const CustomWaitingRoomModal:React.FC<CustomWaitingRoomModalProps> = ({ty
       }}
       backdrop="static"
       keyboard={false}
-      className="modal"  
+      className="modal"
     >
       <Modal.Header closeButton>
-        <Modal.Title>{message['title']}</Modal.Title>
+        <Modal.Title>{message["title"]}</Modal.Title>
       </Modal.Header>
-      <Modal.Body style={{ fontWeight: "lighter" }}>{message['body']}</Modal.Body>
+      <Modal.Body style={{ fontWeight: "lighter" }}>{message["body"]}</Modal.Body>
       <Modal.Footer>
-        <Button variant="pink" className="btn-normal" onClick={()=> setShow(false)}>
+        <Button variant="pink" className="btn-normal" onClick={() => setShow(false)}>
           {t("ok")}
         </Button>
       </Modal.Footer>
