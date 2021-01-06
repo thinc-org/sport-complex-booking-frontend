9eee2abecdacaf61d900d379294d62a75426c6e5
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2021-01-06 21:26:22 +0700

    rebase3

--- src/core/components/pages/staff-pages/staff-management/StaffInterface.tsx
+++
@@ -1,7 +0,0 @@
-export interface admin_and_staff{
-  name: string
-  surname: string
-  username: string
-  password:string
-  is_admin:boolean
-}
---
+++ src/core/components/pages/staff-pages/staff-management/StaffManagementComponents.tsx
@@ -0,0 +1,189 @@
+import React from "react"
+import { Form, Row, Button, Modal } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
+import { passwordSchema } from "./StaffManagementSchema"
+
+export interface admin_and_staff {
+  name: string,
+  surname: string,
+  username: string,
+  password?: string,
+  recheckpasssword?: string | undefined,
+  is_admin: boolean | string,
+}
+
+export interface DeleteStaffProps {
+  show: boolean
+  setShow: (value: boolean) => void
+  mainFunction: (value: admin_and_staff) => void
+  data: admin_and_staff
+}
+
+export const DeleteStaff: React.FC<DeleteStaffProps> = ({ show, setShow, mainFunction, data }) => {
+  return (
+    <Modal
+      show={show}
+      onHide={() => { setShow(false) }}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>คําเตือน</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ fontWeight: "lighter" }}>ท่านกําลังจะลบพนักงานออกจากระบบ ต้องการดําเนินต่อใช่หรือไม่</Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-pink" className="btn-normal"
+          onClick={() => { setShow(false) }}
+        >ยกเลิก</Button>
+        <Button variant="pink" className="btn-normal"
+          onClick={() => { mainFunction(data!) }}
+        >ตกลง</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export interface EditStaffProps {
+  show: boolean
+  setShow: (value: boolean) => void
+}
+
+export const EditStaff: React.FC<EditStaffProps> = ({ show, setShow }) => {
+  return (
+    <Modal
+      show={show}
+      onHide={() => {
+        setShow(false)
+      }}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>คําเตือน</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ fontWeight: "lighter" }}>ท่านได้ทําการเปลี่ยนสถานะของพนักงาน กรุณากด'ตกลง'เพื่อการดําเนินต่อ</Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="pink"
+          className="btn-normal"
+          onClick={() => {
+            setShow(false)
+            window.location.reload();
+          }}
+        >
+          ตกลง
+          </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export interface AddStaffProps {
+  show: boolean
+  setShow: (value: boolean) => void
+  onSubmitAddStaff: (staff: admin_and_staff) => void
+}
+
+export const AddStaff: React.FC<AddStaffProps> = ({ show, setShow, onSubmitAddStaff }) => {
+  const { register, handleSubmit, errors } = useForm({ resolver: yupResolver(passwordSchema) })
+  return (
+    <Modal
+      show={show}
+      onHide={() => {
+        setShow(false)
+      }}
+      backdrop="static"
+      keyboard={true}
+    >
+      <form onSubmit={handleSubmit(onSubmitAddStaff)}>
+        <Modal.Header closeButton>
+          <Modal.Title>เพิ่มพนักงาน</Modal.Title>
+        </Modal.Header>
+        <div className="m-4">
+          <Form.Group>
+            <Row>
+              <Form.Label>ชื่อ</Form.Label>
+              <input type="text" ref={register} name="name" className="form-control" />
+            </Row>
+            {errors.name && <p id="input-error">{errors.name.message}</p>}
+            <Row>
+              <Form.Label>นามสกุล</Form.Label>
+              <input type="text" ref={register} name="surname" className="form-control" />
+            </Row>
+            {errors.surname && <p id="input-error">{errors.surname.message}</p>}
+            <Row>
+              <Form.Label>ชื่อผู้ใช้</Form.Label>
+              <input type="text" ref={register} name="username" className="form-control" />
+            </Row>
+            {errors.username && <p id="input-error">{errors.username.message}</p>}
+            <Row>
+              <Form.Label>รหัสผ่าน</Form.Label>
+              <input type="text" ref={register} name="password" className="form-control" />
+            </Row>
+            {errors.password && <p id="input-error">{errors.password.message}</p>}
+            <Row>
+              <Form.Label>กรอกรหัสผ่านอีกครั้ง</Form.Label>
+              <input type="text" ref={register} name="recheckpassword" className="form-control" />
+            </Row>
+            {errors.recheckpassword && <p id="input-error">{errors.recheckpassword.message}</p>}
+            <Row>
+              <Form.Label>ประเภท</Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                ref={register}
+                name="is_admin"
+              >
+                <option disabled value="ประเภท">ประเภท</option>
+                <option value="สตาฟ">สตาฟ</option>
+                <option value="แอดมิน">แอดมิน</option>
+              </Form.Control>
+            </Row>
+          </Form.Group>
+        </div>
+        <Modal.Footer>
+          <Button
+            variant="outline-pink"
+            className="btn-normal"
+            onClick={() => { setShow(false) }}
+          > ยกเลิก
+        </Button>
+          <Button
+            type="submit"
+            variant="pink"
+            className="btn-normal"
+          >เพิ่ม
+        </Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  )
+}
+
+export interface HandleErrorProps {
+  show: boolean
+  setShow: (value: boolean) => void
+}
+
+export const HandleError: React.FC<HandleErrorProps> = ({ show, setShow }) => {
+  if (!show) return null
+  return (
+    <Modal
+      show={show}
+      onHide={() => { setShow(false) }}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>คําเตือน: เกิดเหตุขัดข้อง</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ fontWeight: "lighter" }}>กรุณาลองใหม่อีกครั้ง</Modal.Body>
+      <Modal.Footer>
+        <Button variant="pink" className="btn-normal"
+          onClick={() => { setShow(false) }}
+        >ตกลง</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
