2e3ff1054e93c811310506669d124078d57a8454
Beam <beam7684@gmail.com>
2021-01-06 20:50:14 +0700

    add base for modals

--- src/core/components/pages/staff-pages/all-reservation-pages/DeleteModalComponent.tsx
+++ src/core/components/pages/staff-pages/all-reservation-pages/DeleteModalComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useHistory, useParams } from "react-router-dom"
-import { Modal, Button } from "react-bootstrap"
 import { DeleteModal, UserInfo } from "../interfaces/reservationSchemas"
+import { CustomModal } from "../list-of-all-users-pages/ListOfAllUserModals"
 
 type ConfirmDelInfo = { members: UserInfo[]; requestDelete: () => void }
 
@@ -21,35 +21,15 @@ export const ConfirmDelModal: React.FC<ModalInterface> = ({ showModalInfo, setSh
   const { showConfirmDel } = showModalInfo
   const { requestDelete, members }: ConfirmDelInfo = info!
   return (
-    <Modal
+    <CustomModal
+      title={"คำเตือน"}
       show={showConfirmDel}
-      onHide={() => {
+      body={`การจองดังกล่าวนี้จะถูกลบ\n\n สตาฟกรุณาโทรแจ้งผู้ใช้ตามที่ระบุก่อนกดตกลง\n ${getAllPhoneNumber(members)}`}
+      onCancel={() => {
         setShowModalInfo({ ...showModalInfo, showConfirmDel: false })
       }}
-      backdrop="static"
-      keyboard={false}
-    >
-      <Modal.Header closeButton className="px-4 pt-4">
-        <Modal.Title>คำเตือน</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="px-4" style={{ fontWeight: "lighter", whiteSpace: "pre-line" }}>
-        {"การจองดังกล่าวนี้จะถูกลบ\n\n" + "สตาฟกรุณาโทรแจ้งผู้ใช้ตามที่ระบุก่อนกดตกลง\n" + getAllPhoneNumber(members)}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="outline-secondary"
-          className="btn-normal btn-outline-pink"
-          onClick={() => {
-            setShowModalInfo({ ...showModalInfo, showConfirmDel: false })
-          }}
-        >
-          ยกเลิก
-        </Button>
-        <Button variant="outline-danger" className="btn-normal btn-outline-red" onClick={requestDelete}>
-          ยืนยัน
-        </Button>
-      </Modal.Footer>
-    </Modal>
+      onConfirm={requestDelete}
+    />
   )
 }
 
@@ -58,65 +38,28 @@ export const ComDelModal: React.FC<ModalInterface> = ({ showModalInfo, setShowMo
   const { pagename } = useParams<{ pagename: string; _id: string }>()
   const { showComDel } = showModalInfo
   return (
-    <Modal
+    <CustomModal
+      title={"เสร็จสิ้น"}
       show={showComDel}
-      onHide={() => {
+      body={"ระบบได้ทำการลบการจองเรียบร้อยแล้ว"}
+      onConfirm={() => {
         setShowModalInfo({ ...showModalInfo, showComDel: false })
         history.push(`/staff/allReservation/${pagename}`)
       }}
-      backdrop="static"
-      keyboard={false}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>เสร็จสิ้น</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="px-4" style={{ fontWeight: "lighter" }}>
-        ระบบได้ทำการลบการจองเรียบร้อยแล้ว
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="pink"
-          className="btn-normal"
-          onClick={() => {
-            setShowModalInfo({ ...showModalInfo, showComDel: false })
-            history.push(`/staff/allReservation/${pagename}`)
-          }}
-        >
-          ตกลง
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    />
   )
 }
 
 export const ErrModal: React.FC<ModalInterface> = ({ showModalInfo, setShowModalInfo }) => {
   const { showErr } = showModalInfo
   return (
-    <Modal
+    <CustomModal
+      title={"เกิดข้อผิดพลาด"}
       show={showErr}
-      onHide={() => {
+      body={"ไม่สามารถลบการจองได้ในขณะนี้"}
+      onConfirm={() => {
         setShowModalInfo({ ...showModalInfo, showErr: false })
       }}
-      backdrop="static"
-      keyboard={false}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>เกิดข้อผิดพลาด</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="px-4" style={{ fontWeight: "lighter" }}>
-        ไม่สามารถลบการจองได้ในขณะนี้
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="pink"
-          className="btn-normal"
-          onClick={() => {
-            setShowModalInfo({ ...showModalInfo, showErr: false })
-          }}
-        >
-          ตกลง
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    />
   )
 }
