b176829c4872186af0201258bd0342409c18a8b1
Dhanabordee Mekintharanggur <pointmekin@gmail.com>
2021-01-08 21:37:48 +0700

    remove '?' from endpoint url

--- src/core/components/pages/staff-pages/staff-management/StaffManagement.tsx
+++ src/core/components/pages/staff-pages/staff-management/StaffManagement.tsx
@@ -78,7 +78,7 @@ export default function StaffManagement() {
     const end = pageNo * 10
     const query_filter = query ? query : "$"
     const type_filter = type ? type : "all"
-    await client.get<admin_and_staff[]>('/staff-manager/admin-and-staff/search?',{
+    await client.get<admin_and_staff[]>('/staff-manager/admin-and-staff/search',{
       params: {
         start:start,
         end: end,
