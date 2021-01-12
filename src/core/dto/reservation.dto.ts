3191479cc084e80ad0ecb44d3ab1a0577d4d5eb8
Suparuek Saetoen <suparuk1902@hotmail.com>
2021-01-12 15:12:55 +0700

    resolve conflict

--- src/core/dto/reservation.dto.ts
+++ src/core/dto/reservation.dto.ts
@@ -1,6 +1,5 @@
 import { Account } from "../components/pages/staff-pages/interfaces/InfoInterface"
 import { CuStudent, Other, SatitCuPersonel } from "../contexts/UsersContext"
-import { Account } from "../components/pages/staff-pages/interfaces/InfoInterface"
 
 export type Room = SuccessRoom | WaitingRoom
 
@@ -89,57 +88,4 @@ export interface ReservationResponse {
   time_slot: number[]
   day_of_week: number
   list_member: (CuStudent | SatitCuPersonel | Other)[]
-}
-
-export type Room = SuccessRoom | WaitingRoom
-
-export type ReserveListRes = [number, Room[]]
-
-export interface MemberInfoRes {
-  account_type: Account
-  personal_email: string
-  phone: string
-  username: string
-  _id: string
-}
-
-export interface DefaultRoom {
-  court_number: number
-  date: Date
-  list_member: MemberInfoRes[]
-  sport_id: string
-  time_slot: number[]
-  _id: string
-}
-
-export interface SuccessRoom extends DefaultRoom {
-  is_check: boolean
-}
-
-export interface WaitingRoom extends DefaultRoom {
-  access_code: string
-  day_of_week: number
-  expired_date: Date
-}
-
-export interface TimeObject {
-  startHour: string
-  startMinute: string
-  endHour: string
-  endMinute: string
-}
-
-export interface Sport {
-  _id: string
-  sport_name_th: string
-  sport_name_en: string
-  required_user: number
-  quota: number
-  list_court: [Court]
-}
-
-export interface Court {
-  court_num: number
-  open_time: number
-  close_time: number
 }
