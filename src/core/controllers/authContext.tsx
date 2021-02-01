35bd89003d70101d5e445cd318bc9c062e6da5ca
siradheek <siradhee_k@hotmail.com>
2021-02-01 18:12:08 +0700

    move admin check to useAuthContext

--- src/core/controllers/authContext.tsx
+++ src/core/controllers/authContext.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react"
 import jwt_decode from "jwt-decode"
 import { getCookie } from "../contexts/cookieHandler"
+import { client } from "../../axiosConfig"
 interface AuthContextConstruct {
   token: string | undefined
   isUser: boolean
@@ -8,22 +9,39 @@ interface AuthContextConstruct {
   isAdmin: boolean
   setIsAdmin: (isAdmin: boolean) => void
 }
+interface Token {
+  isStaff: boolean
+  exp: number
+  iat: number
+  userId: string
+}
 
 export const AuthContext = React.createContext({} as AuthContextConstruct)
 
 export const useAuthContext = () => useContext(AuthContext)
 
 const AuthProvider = ({ ...props }) => {
-  const [token, setToken] = useState(getCookie("token") ?? "")
+  const [token, setToken] = useState(getCookie("token") ?? undefined)
   const [isAdmin, setIsAdmin] = useState(false)
-  const isUser = !!(token ? jwt_decode(token) : "")
+  const decodedToken = token ? jwt_decode<Token>(token) : undefined
+  const isUser = !!decodedToken
+  const isStaff = decodedToken?.isStaff
   const value = { token, isUser, setToken, isAdmin, setIsAdmin }
-
+  useEffect(() => {
+    if (isStaff) {
+      client
+        .get("staffs/profile")
+        .then((res) => {
+          if (res.data.is_admin) setIsAdmin(true)
+          else setIsAdmin(false)
+        })
+        .catch((err) => setIsAdmin(false))
+    }
+  }, [setIsAdmin, isStaff])
   useEffect(() => {
     const cookieToken = getCookie("token") ?? ""
     setToken(cookieToken)
   }, [isUser])
-
   return <AuthContext.Provider value={value} {...props} />
 }
 
