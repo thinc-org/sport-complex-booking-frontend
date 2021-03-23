f1de9d9f8c4828a54a8b6af724917941ae370b41
siradheek <siradhee_k@hotmail.com>
2021-03-23 21:15:48 +0700

    remove unneccessary imports

--- src/core/components/ui/navbar/staff-navbar.tsx
+++ src/core/components/ui/navbar/staff-navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Navbar, NavbarBrand, Button } from "react-bootstrap"
-import { Link, useLocation, useHistory } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import { useAuthContext } from "../../../controllers/authContext"
 import { setCookie } from "../../../contexts/cookieHandler"
 import { useNavHeader } from "./navbarSideEffect"
@@ -9,7 +9,6 @@ import logo from "../../../assets/images/logo.png"
 function StaffNavbar() {
   const { isOnStaffLoginPage, isOnStaffPage } = useNavHeader()
   const { setToken } = useAuthContext()
-  const location = useLocation()
   const history = useHistory()
 
   const onLogout = async () => {
