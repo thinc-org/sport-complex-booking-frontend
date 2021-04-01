fb4c2e010598879596125e9b10e3e03e7e23f239
siradheek <siradhee_k@hotmail.com>
2021-04-01 19:57:49 +0700

    add translation for navHeader

--- src/core/components/pages/LandingComponent.tsx
+++ src/core/components/pages/LandingComponent.tsx
@@ -10,7 +10,7 @@ function Landing() {
 
   return (
     <>
-      <NavHeader isOnStaffPage={true} isOnStaffLoginPage={true} />
+      <NavHeader isOnStaffPage={true} isOnStaffLoginPage={true} header={t("cuHeader")} />
       <div className="container landing" style={{ backgroundColor: "var(--bg-color)" }}>
         <div className="row landing" style={{ height: "100vh" }}>
           <div className="d-none d-sm-block my-auto col-sm-8 textgroup landing">
