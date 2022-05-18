b850a2e7969ef2d393c3a3a8eba8f0d57b8abb74
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2022-05-18 18:26:37 +0700

    Change QR code regeneration interval to 40 seconds

--- src/core/components/pages/Reservation/ReservationDetail/QRCode.tsx
+++ src/core/components/pages/Reservation/ReservationDetail/QRCode.tsx
@@ -7,7 +7,7 @@ import { TWO_HOURS_MILLISECOND } from "../../../../../constant"
 export const QrCode: React.FC<QRCodeProps> = ({ isCheck, id, validTime, setValidTime, reservedTimeInMillisecond }) => {
   const { t } = useTranslation()
 
-  const [counter, setCounter] = useState<number>(10)
+  const [counter, setCounter] = useState<number>(40)
   const [allowCheckin, setAllowCheckin] = useState<boolean>(false)
 
   const countDown = useCallback(() => {
@@ -17,7 +17,7 @@ export const QrCode: React.FC<QRCodeProps> = ({ isCheck, id, validTime, setValid
           setCounter(counter - 1)
         } else if (counter === 0) {
           setValidTime(new Date().getTime() + 10000)
-          setCounter(10)
+          setCounter(40)
         }
       }, 1000)
     }
@@ -58,7 +58,7 @@ export const QrCode: React.FC<QRCodeProps> = ({ isCheck, id, validTime, setValid
         <h6 className="mt-2 mb-0" style={{ fontWeight: 400 }}>
           {t("qrcodeInvalid")}{" "}
         </h6>
-        <div style={{ fontSize: "18px", fontWeight: 400 }}> 00:{counter === 10 ? counter : "0" + counter} </div>
+        <div style={{ fontSize: "18px", fontWeight: 400 }}> 00:{counter >= 10 ? counter : "0" + counter} </div>
         <QRCode className="mb-4 mt-3" value={validTime + "/" + id} renderAs="svg" size={128} fgColor="#333" bgColor="#fff" />
 
         <h5 className="mb-2" style={{ fontWeight: 400 }}>
